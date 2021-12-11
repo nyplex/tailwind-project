e6bff6555abadc0b0c85fb299402c60df9cfa298
Alex <nypels.alexandre@outlook.com>
2021-12-11 20:50:37 +0000

    Adding tailwind Grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["src/**/*.{html,js, css}", "public/**/*.{html,js, css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontSize: {
+        mammoth: '8rem'
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
